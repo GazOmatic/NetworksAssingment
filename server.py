18a911c16d7d1d08a4c44d5f6bc51e288ebf19ed
Gaz <garethw738@gmail.com>
2023-02-24 12:02:31 +0200

    Did more

--- server.py
+++ server.py
@@ -5,27 +5,41 @@ controlHost = ""
 controlPort = 420
 
 
+def clientThread():
+    print("stuff")
+    server(controlHost, 1000)
+
+
 def negotiate(conn):
+    port = 1000
     conn.sendall(b"PORT:1000")
+    t = threading.Thread(target=clientThread, daemon=True)
+    t.start()
     return True
 
+
 def server(HOST, PORT):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+        server.bind((HOST, PORT))
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server.listen()
+        conn, addr = server.accept()
         with conn:
             print(f"Connected by {addr}")
             while True:
                 if PORT == 420:
                     if negotiate(conn):
-                        s.close()
                         time.sleep(2)
                         print("Not closed")
-                # data = conn.recv(1024)
+                        break
+                else:
+                    data = conn.recv(1024)
+
+            server.close()
+            server.
 
 
 while True:
-    server(controlHost,controlPort)
+    server(controlHost, controlPort)
     print("Resetting control port")
-    time.sleep(2)
+    time.sleep(1)
