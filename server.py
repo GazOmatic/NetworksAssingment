b9db30c59ba7d74bd37d0c091a13da2179b3db47
Gaz <garethw738@gmail.com>
2023-02-26 17:06:01 +0200

    Added some commenting

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import random
 from portManager import *
 controlHost = ""
 controlPort = 420
+usedPorts = []
+currentPort = 1000
 
 
 def clientThread(newPort):
@@ -22,28 +24,37 @@ def clientThread(newPort):
             conn.close()
 
 
-def negotiate(conn):
-    port = getPort()
-    conn.sendall(f"PORT:{port}".encode())
-    t = threading.Thread(target=clientThread, daemon=True, args=(port,))
+
+
+def negotiate(conn): # This function is what will determine what ports are available and can be used for a new client.
+    global currentPort
+    while currentPort in usedPorts:
+        currentPort = currentPort + 1
+        if currentPort == 2000:
+            currentPort = 1000
+    conn.sendall(f"PORT:{currentPort}".encode())
+    t = threading.Thread(target=clientThread, daemon=True,args=(currentPort,))
     t.start()
+    usedPorts.append(currentPort)
     return True
 
 
-def server(sock, HOST, PORT):
+def server(sock,HOST,PORT): # Server accepts any incoming connection 
     conn, addr = sock.accept()
     with conn:
         print(f"Connected by {addr} on {PORT}")
         while True:
-            if PORT == 420:
+            if PORT == 420: # If a client connects to control port (420) It means it neads to negotiate a new port
                 if negotiate(conn):
                     print("Negotiated port")
                     break
         conn.close()
 
 
+# Begining of the server
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((controlHost, controlPort))
+    sock.bind((controlHost, controlPort)) # Server listens on 420 first
     sock.listen()
     while True:
         server(sock, controlHost, controlPort)
