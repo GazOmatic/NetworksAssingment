563486ebf28362629d77fd4255bf07cff0c0ed7c
Gaz <garethw738@gmail.com>
2023-02-28 09:56:37 +0200

    Did a bunch of fixup

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Define the address and port to connect to
 host = 'localhost'  # The remote host IP address
-port = 420       # The remote host port number
+port = 3000       # The remote host port number
 
 # Connect to the server
 sock.connect((host, port))
@@ -13,10 +13,11 @@ sock.connect((host, port))
 # Receive data from the server
 while True:
     received_data = sock.recv(1024).decode()
+    print(f"Server: {received_data}")
     message = input(":")
     if len(message) == 0:
         message = "#"
-    sock.sendall(input("#").encode())
+    sock.sendall(message.encode())
 
 
 sock.close()
--- server.py
+++ server.py
@@ -1,30 +1,37 @@
 import socket
-import time
 import threading
-import random
 controlHost = ""
-PORT = 420
+PORT = 3000
 
-def processHeader(header):
-   print("Hello world") 
+def processHeader(header,conn):
+   print(f"Client: {header}")
+   output = "sdjskdjk"
+   conn.sendall(output.encode())
 
 
-def clientThread():
-    conn, addr = clientSocket.accept()
-    t = threading.Thread(target=clientThread, daemon=True)
-    t.start()
+def clientThread(conn):
     with conn:
         print(f"Connected by {addr} on {PORT}")
         conn.sendall("Hello from server".encode())
         while True:
             data = conn.recv(1024)
-            response = "##"
-            conn.sendall(response.encode())
-            print(data)
+            if not data:
+                break
+            processHeader(data,conn)
+
+threads = []
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
         clientSocket.bind((controlHost, PORT))
         clientSocket.listen()
         while True:
-            clientThread()
+            try:
+                conn, addr = clientSocket.accept()
+                t = threading.Thread(target=clientThread, daemon=True, args=(conn,))
+                t.start()
+                threads.append(t)
+                print(len(threads))
+            except KeyboardInterrupt:
+                print("DONe")
+    
