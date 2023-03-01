07acbccb25ca04dc939e6a4fbeabc510b6fa4a32
Gaz <garethw738@gmail.com>
2023-03-01 23:03:55 +0200

    Redid the way the server keeps track of threads

--- server.py
+++ server.py
@@ -7,17 +7,21 @@ controlHost = ""
 PORT = 3000
 
 BATCH = 1024
-threads = []
+
+threads = 0
 
 
 def clientThread(conn: socket.socket):
     with conn:
         print(f"Connected by {addr} on {PORT}")
-        man = connectionManager(False,conn,BATCH) # create a new connection manager and set to not sending
+        # create a new connection manager and set to not sending
+        man = connectionManager(False, conn, BATCH)
         while True:
             out = man.next("Hello world")
-            if out == 0: # If it could not send the data, terminate the current thread
-                break
+            if out == 0:  # If it could not send the data, terminate the current thread
+                break  # Escape the loop if message failed
+    threads = threads - 1
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
     clientSocket.bind((controlHost, PORT))
@@ -27,10 +31,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
             conn, addr = clientSocket.accept()
             print(type(conn))
             # When connection is created fork the thread and then repeat
-            t = threading.Thread(target=clientThread, args=(conn,))
-            t.daemon = True
-            t.start()
-            threads.append(t)
+            client = threading.Thread(target=clientThread, args=(conn,))
+            # client.daemon = True
+            client.start()
+            # Naming of threads for debugging reasons
+            client.name = f"Client{len(threads)}"
+            threads = threads + 1
             print(f"Active Clients {len(threads)}")
         except KeyboardInterrupt:
             print("Done")
