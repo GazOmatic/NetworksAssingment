dcc559e3d7d01545284da5d381fcd87cbdc6071f
Gaz <garethw738@gmail.com>
2023-03-01 19:23:16 +0200

    Overhauled the client and server to use the connectionManager

--- client.py
+++ client.py
@@ -6,32 +6,35 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Define the address and port to connect to
 host = 'localhost'  # The remote host IP address
 port = 3000       # The remote host port number
+sending = True
 
-# Connect to the server
+BATCH = 1024
 sock.connect((host, port))
 
+def receive(data:str):
+    if sending == True:
+        return send(data)
+    else:
+      return sock.recv(BATCH).decode()  
+
+def send(data:str):
+    print()
+    if sending == False:
+        return receive()
+    else:
+        return sock.sendall(data.encode())
+
+def get(filename:str):
+    print()
+
+# Connect to the server
 print("Welcome to CLS File Sharing Platform")
 print("Type HELP for list of commands")
 
 
-def get():
-    header = "GET#zero.py#"
-    return header
-
-
-while True:
-    # Receive data from the server
-    received_data = sock.recv(1024).decode()
-    print(f"Server: {received_data}")
-
-    # Send data to server
-    # message = input(":")
-
-    message = get()
-    if len(message) == 0:
-        message = "#"
-    #
-    sock.sendall(message.encode())
-    input("#")
-
-sock.close()
+command = ''
+while command != 'q':
+    print("GET, LIST, TEST")
+    command = input("#")
+    if command == 'GET':
+          get("Files/zero.py")
--- server.py
+++ server.py
@@ -1,98 +1,35 @@
 import socket
 import threading
 import os
+import connectionManager
 controlHost = ""
 PORT = 3000
 
-
-def processHeader(message, conn):
-    print(f"Client: {message}")
-    msg = message.split("#")
-    if msg[0] == "LIST":
-        actionList(msg)
-    elif msg[0] == "GET":
-        actionGet(msg)
-    elif msg[0] == "POST":
-        actionPost(msg)
-    elif msg[0] == "HELP":
-        actionHelp(msg)
-    else:
-        conn.sendall("Unknown Server Command".encode())
-    output = "Output"
-    conn.sendall(output.encode())
-
-
-# This method lists all files to the client
-def actionList(message):
-    if message[1] == "":
-        fileList = ""
-        files = os.listdir("Files/")
-        for f in files:
-            fileList += f + "\n"
-
-        conn.sendall(fileList.encode())
-    elif message[1] == "ACCESS":
-        conn.sendall("List all files I have access to".encode())
-    else:
-        conn.sendall("Unknown Server Command".encode())
-
-
-def actionGet(message):  # TODO
-    print("GET")
-    filename = "Files/" + message[1]
-    with open(filename,"rb") as fileToSend:
-        fileSize = os.path.getsize(filename) # Get the size of the file to send
-        conn.send(str(fileSize).encode()) # Send the size of the file through
-        bytesSent = 0
-        while bytesSent < fileSize:
-            remainingBytesToSend = fileSize - bytesSent # Determine how many bytes are left to send
-            amoutToSend = min(remainingBytesToSend, 1024) # Send the size of the file. If the size of the file is less than the normal bytes size to send then send the byte size
-            bytesSent += conn.sendfile(fileToSend,bytesSent,amoutToSend)
-            
-            conn.sendfile(fileToSend)
-
-
-
-def actionPost(message):  # TODO
-    print("POST")
-
-
-# This function prints a list of all commands the server accepts to the client
-
-
-def actionHelp(message):
-    conn.sendall(
-        "Commands:\nList all files: LIST\nUpload file: POST\nDownload file: GET".encode())
-
-
-def clientThread(conn):
-    with conn:
-        print(f"Connected by {addr} on {PORT}")
-        conn.sendall("Hello from server".encode())
-        while True:
-            # decoded
-            data = conn.recv(1024).decode()
-            if not data:
-                break
-            processHeader(data, conn)
-
-
+BATCH = 1024
 threads = []
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
-        clientSocket.bind((controlHost, PORT))
-        clientSocket.listen()
-        while True:
-            try:
-                conn, addr = clientSocket.accept()
-                # When connection is created fork the thread and then repeat
-                t = threading.Thread(target=clientThread, args=(conn,))
-                t.daemon = True
-                t.start()
-                threads.append(t)
-                print(len(threads))
-            except KeyboardInterrupt:
-                print("Done")
-                socket.close()
+def clientThread(conn: socket.socket):
+    with conn:
+        print(f"Connected by {addr} on {PORT}")
+        man = connectionManager(False,conn,BATCH) # create a new connection manager and set to not sending
+        man.sen
+
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
+    clientSocket.bind((controlHost, PORT))
+    clientSocket.listen()
+    while True:
+        try:
+            conn, addr = clientSocket.accept()
+            print(type(conn))
+            # When connection is created fork the thread and then repeat
+            t = threading.Thread(target=clientThread, args=(conn,))
+            t.daemon = True
+            t.start()
+            threads.append(t)
+            print(len(threads))
+        except KeyboardInterrupt:
+            print("Done")
+            clientSocket.close()
