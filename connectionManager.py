78f542e6598e00a278b9b7d24388c2e5232d0d8e
Gaz <garethw738@gmail.com>
2023-03-01 19:49:57 +0200

    Updated connManager

--- connectionManager.py
+++ connectionManager.py
@@ -1,20 +1,24 @@
 import socket
 
+
 class connectionManager:
     def __init__(self, sending: bool, sock: socket.socket, BATCH: int) -> None:
         self.sending = sending
         self.sock = sock
         self.BATCH = BATCH
 
-    def receive(self, data: str):
+    def next(self, data: str):
         if self.sending == True:
+            sending = False
             return self.send(data)
         else:
-            return self.sock.recv(self.BATCH).decode()
+            sending = True
+            return self.receive(data)
+
+    def receive(self, data: str):
+        data = self.sock.recv(self.BATCH).decode()
+        return data
 
     def send(self, data: str):
-        print()
-        if self.sending == False:
-            return self.receive()
-        else:
-            return self.sock.sendall(data.encode())
+        self.sock.sendall(data.encode())
+        return "Send success"
