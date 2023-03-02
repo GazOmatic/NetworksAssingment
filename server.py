85e45d68180a260d13f6a41ce588c80b32fe3e71
Gaz <garethw738@gmail.com>
2023-03-02 09:11:50 +0200

    Made the server usse the main function

--- server.py
+++ server.py
@@ -14,30 +14,36 @@ threads = 0
 def clientThread(conn: socket.socket):
     global threads
     with conn:
-        print(f"Connected by {addr} on {PORT}")
         # create a new connection manager and set to not sending
         man = connectionManager(False, conn, BATCH)
         while True:
             out = man.next("Hello world")
             if out == 0:  # If it could not send the data, terminate the current thread
-                threads = threads - 1 # Decrement the thread count
+                threads = threads - 1  # Decrement the thread count
                 break  # Escape the loop if message failed
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
-    clientSocket.bind((controlHost, PORT))
-    clientSocket.listen()
-    while True:
-        try:
-            conn, addr = clientSocket.accept()
-            # When connection is created fork the thread and then repeat
-            client = threading.Thread(target=clientThread, args=(conn,))
-            client.daemon = True
-            client.start()
-            # Naming of threads for debugging reasons
-            client.name = f"Client{threads}"
-            threads = threads + 1
-            print(f"Active Clients {threads}")
-        except KeyboardInterrupt:
-            print("Done")
-            clientSocket.close()
+def main():
+    global threads, controlHost
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
+        clientSocket.bind((controlHost, PORT))
+        clientSocket.listen()
+        while True:
+            try:
+                conn, addr = clientSocket.accept()
+                print(f"Connected by {addr} on {PORT}")
+                # When connection is created fork the thread and then repeat
+                client = threading.Thread(target=clientThread, args=(conn,))
+                client.daemon = True
+                client.start()
+                # Naming of threads for debugging reasons
+                client.name = f"Client{threads}"
+                threads = threads + 1
+                print(f"Active Clients {threads}")
+            except KeyboardInterrupt:
+                print("Done")
+                clientSocket.close()
+
+
+if __name__ == "__main__":
+    main()
