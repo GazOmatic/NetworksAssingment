5f76bac6cb5c8364996d63ba4ccc1d27933729b1
richietaylor <richard@taylorfamily.co.za>
2023-03-05 15:35:03 +0200

    Added encryption and decryption

--- connectionManager.py
+++ connectionManager.py
@@ -1,5 +1,6 @@
 import socket
 BATCH = 4096
+KEY = 3
 
 
 class connectionManager:
@@ -21,7 +22,7 @@ class connectionManager:
 
     def receive(self, BATCH=BATCH):  # Function that will receive data
         try:
-            data = self.sock.recv(self.BATCH)
+            data = self.decrypt(self.sock.recv(self.BATCH).decode())
         except ConnectionResetError:
             print("Error lost connection!")
             return 0
@@ -32,7 +33,9 @@ class connectionManager:
     def send(self, data: str):  # Function that will send the data
         try:
             if type(data) != bytes:
+                data = self.encrypt(data)
                 data = data.encode()
+
             self.sock.sendall(data)
         except ConnectionResetError:
             print("Error lost connection!")
@@ -41,3 +44,37 @@ class connectionManager:
             print("Connection aborted")
             return 0
         return 1
+
+    def encrypt(self, message: str):
+        """
+        Encrypts a message using a shift cipher with the given key.
+        """
+        global KEY
+        ciphertext = ""
+        for char in message:
+            if char.isalpha():
+                # Shift the character by the key, wrapping around if necessary
+                shifted_char = chr(
+                    (ord(char) - ord('a') + KEY) % 26 + ord('a'))
+                ciphertext += shifted_char
+            else:
+                # Leave non-alphabetic characters unchanged
+                ciphertext += char
+        return ciphertext
+
+    def decrypt(self, ciphertext: str):
+        """
+        Decrypts a ciphertext using a shift cipher with the given key.
+        """
+        global KEY
+        message = ""
+        for char in ciphertext:
+            if char.isalpha():
+                # Shift the character by the inverse of the key, wrapping around if necessary
+                shifted_char = chr(
+                    (ord(char) - ord('a') - KEY) % 26 + ord('a'))
+                message += shifted_char
+            else:
+                # Leave non-alphabetic characters unchanged
+                message += char
+        return message
